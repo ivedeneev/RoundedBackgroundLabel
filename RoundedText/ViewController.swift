b4594c1b71fc7f35007c3f55c59058a54da318b5
Igor Vedeneev <getmaxx@hotmail.com>
2025-02-18 10:21:22 +0100

    Initial Commit

---
+++ RoundedText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RoundedText
+//
+//  Created by Igor Vedeneev on 18/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
